b2f280656fdb94fd07a549dbbf9027405c48d7a9
r <rmijic@gmail.com>
2019-06-13 16:44:43 +0200

    Added date constraint to ether supply
    date constraint results in a quick, cheap query which gives data for the past 7 days.

--- views/ether-supply-by-date.sql
+++ views/ether-supply-by-date.sql
@@ -4,15 +4,14 @@ with ether_emitted_by_date AS (
     SUM(value) AS value
   FROM
     `bigquery-public-data.ethereum_blockchain.traces`
-  WHERE
-    trace_type IN ('genesis',
-      'reward')
+  WHERE TRUE
+    AND ( trace_type IN ('genesis','reward') )
+    AND ( DATE(block_timestamp) >= DATE_ADD(CURRENT_DATE(), INTERVAL -7 DAY ) )
   GROUP BY
     DATE(block_timestamp) )
 
 SELECT
   date,
-  SUM(value) OVER (ORDER BY date) / power(10,
-    18) AS supply
+  SUM(value) OVER (ORDER BY date) / power(10, 18) AS supply
 FROM
   ether_emitted_by_date
